de66f41c4b69846bfbc4216512e2770157180124
vluthman <vluthman@kth.se>
2022-02-08 17:54:31 +0100

    Now works for Windows as well #8

--- src/main/java/compile.java
+++ src/main/java/compile.java
@@ -3,20 +3,32 @@ import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 public class compile {
+	public static void main(String[] args) {
+		try {
+			boolean isWindows = isWindows();
+			Process process;
+			String[] commands;
+			if (isWindows) {
+				String command = "cmd.exe /c echo 1";
+				process = Runtime.getRuntime().exec(command);
+			} else {
+				commands = new String[]{"sh", "-c", "echo 1"};
+				process = Runtime.getRuntime().exec(commands);
+			}
 
-    public static void main(String[] args) {
-        try {
-            String[] commands = {"sh", "-c", "echo 1"};
-            Process process = Runtime.getRuntime().exec(commands);
-            String result = new BufferedReader(new InputStreamReader(process.getInputStream()))
-                    .lines().collect(Collectors.joining("\n"));
-            String error = new BufferedReader(new InputStreamReader(process.getErrorStream()))
-                    .lines().collect(Collectors.joining("\n"));
-            //System.out.println(result);
-            //System.out.println(error);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+			String result = new BufferedReader(new InputStreamReader(process.getInputStream()))
+					.lines().collect(Collectors.joining("\n"));
+			String error = new BufferedReader(new InputStreamReader(process.getErrorStream()))
+					.lines().collect(Collectors.joining("\n"));
+			//System.out.println(result);
+			//System.out.println(error);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private static boolean isWindows() {
+		return System.getProperty("os.name").toLowerCase().startsWith("windows");
+	}
 
 }

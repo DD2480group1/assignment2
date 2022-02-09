09a89e156ad21a911b1da90c4e812025be8a8e1b
vluthman <vluthman@kth.se>
2022-02-09 14:32:56 +0100

    Change method name

--- src/main/java/compile.java
+++ src/main/java/compile.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 public class compile {
-	public static void main(String[] args) {
+	public static void compileProject() {
 		try {
 			boolean isWindows = isWindows();
 			Process process;

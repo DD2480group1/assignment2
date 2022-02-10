a6461d1918acf1d1dbfbe56dd7a4afe2203140fd
Isabel Redtzer <isabel.redtzer@gmail.com>
2022-02-10 15:36:55 +0100

    Added test structure to JsonTest #16

---
+++ src/test/java/JsonTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+        import javax.json.JsonObject;
+        import static org.junit.Assert.assertTrue;
+
+public class JsonTest {
+
+    //Tests if the RepoURL is retrieved correctly from test file
+    @Test
+    public void RepoURLTest(){
+        //TODO
+    }
+
+    //Tests if the email is retrieved correctly from test file
+    @Test
+    public void EmailTest(){
+        //TODO
+    }
+
+    //Tests if the commit-ID is retrieved correctly from test file
+    @Test
+    public void HeadCommitIdTest(){
+        //TODO
+    }
+
+    //Tests if the Commit Message is retrieved correctly from test file
+    @Test
+    public void CommitMsgTest(){
+        //TODO
+    }
+}

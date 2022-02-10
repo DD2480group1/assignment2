e504f1035ca669d887c57500b8307810143b8895
Isabel Redtzer <isabel.redtzer@gmail.com>
2022-02-10 22:10:46 +0100

    Changed handle to override function #17

--- src/main/java/SkeletonCode.java
+++ src/main/java/SkeletonCode.java
@@ -19,41 +19,6 @@ import java.util.stream.Collectors;
  * See the Jetty documentation for API documentation of those classes.
  */
 class SkeletonCode extends AbstractHandler {
-    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
-
-        System.out.println(target);
-
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
-
-
-        BufferedReader reader = new BufferedReader(request.getReader());
-        String payload = reader.lines().collect(Collectors.joining());
-        reader.close();
-
-        JsonObject obj = JsonUtil.getJson(payload);
-        String commitMsg = JsonUtil.getCommitMsg(obj);
-        System.out.println(commitMsg);
-        String url = JsonUtil.getRepoUrl(obj);
-
-        boolean cloneSucceeded = CloneRepo.cloneRepo(url);
-        if (cloneSucceeded) {
-            System.out.println("Successfully cloned repo from" + url);
-        } else {
-            System.out.println("Failed to clone repo from" + url);
-        }
-
-        compile.compileProject();
-
-        response.getWriter().println("CI job done");
-    }
-
     /**
      * Function to test read our test.json file (see that everything works)
      * @param path
@@ -77,10 +42,45 @@ class SkeletonCode extends AbstractHandler {
 
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
+        Server server = new Server(8081);
         server.setHandler(new SkeletonCode());
         server.start();
         server.join();
+    }
+
+    @Override
+    public void handle(String s, Request request, jakarta.servlet.http.HttpServletRequest httpServletRequest, jakarta.servlet.http.HttpServletResponse httpServletResponse) throws IOException, jakarta.servlet.ServletException {
+        httpServletResponse.setContentType("text/html;charset=utf-8");
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        request.setHandled(true);
+
+        System.out.println(s);
+
+        // here you do all the continuous integration tasks
+        // for example
+        // 1st clone your repository
+        // 2nd compile the code
+
+
+        BufferedReader reader = new BufferedReader(httpServletRequest.getReader());
+        String payload = reader.lines().collect(Collectors.joining());
+        reader.close();
+
+        JsonObject obj = JsonUtil.getJson(payload);
+        String commitMsg = JsonUtil.getCommitMsg(obj);
+        System.out.println(commitMsg);
+        String url = JsonUtil.getRepoUrl(obj);
+
+        boolean cloneSucceeded = CloneRepo.cloneRepo(url);
+        if (cloneSucceeded) {
+            System.out.println("Successfully cloned repo from" + url);
+        } else {
+            System.out.println("Failed to clone repo from" + url);
+        }
+
+        compile.compileProject();
+
+        httpServletResponse.getWriter().println("CI job done");
     }
 }
 

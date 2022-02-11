34acdf4838bd2ffe6e3ad5cf4dd410ebc67753df
Isabel Redtzer <isabel.redtzer@gmail.com>
2022-02-11 10:26:21 +0100

    Added testRepo-function. Compile and test are now printing results to terminal #38

--- src/main/java/compile.java
+++ src/main/java/compile.java
@@ -12,21 +12,43 @@ public class compile {
 				String command = "cmd.exe /c cd repo/ & mvn compile & echo %errorlevel%";
 				process = Runtime.getRuntime().exec(command);
 			} else {
-				String[] commands = new String[]{"sh", "-c","cd repo/ mvn compile; echo $?"};
+				String[] commands = new String[]{"sh", "-c","cd repo; mvn compile; echo $?"};
 				process = Runtime.getRuntime().exec(commands);
 			}
 
 			String result = new BufferedReader(new InputStreamReader(process.getInputStream()))
 					.lines().collect(Collectors.joining("\n"));
 
+			System.out.println(result);
 			String[] lines = result.split("\n");
 			errorCode = Integer.parseInt(lines[lines.length-1]);
 
-			if (errorCode == 0) {
-				System.out.println("Ran successfully");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return errorCode;
+	}
+
+	public static int testRepo(){
+		int errorCode = -1;
+		try {
+			boolean isWindows = isWindows();
+			Process process;
+			if (isWindows) {
+				String command = "cmd.exe /c cd repo/ & mvn test -q & echo %errorlevel%";
+				process = Runtime.getRuntime().exec(command);
 			} else {
-				System.out.println("Failed");
+				String[] commands = new String[]{"sh", "-c","cd repo; mvn test -q; echo $?"};
+				process = Runtime.getRuntime().exec(commands);
 			}
+
+			String result = new BufferedReader(new InputStreamReader(process.getInputStream()))
+					.lines().collect(Collectors.joining("\n"));
+
+			System.out.println(result);
+			String[] lines = result.split("\n");
+			errorCode = Integer.parseInt(lines[lines.length-1]);
+
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
@@ -35,5 +57,9 @@ public class compile {
 
 	private static boolean isWindows() {
 		return System.getProperty("os.name").toLowerCase().startsWith("windows");
+	}
+	public static void main(String[] args){
+		compileProject();
+		testRepo();
 	}
 }

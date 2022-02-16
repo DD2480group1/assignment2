ac8bf8d458726818a974dd6810aa70bf5b756bae
rubbestad <rubbestads@gmail.com>
2022-02-16 11:55:59 +0100

    Website styling

--- src/main/java/HtmlBuilder.java
+++ src/main/java/HtmlBuilder.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.util.List;
 
 public class HtmlBuilder {
+
+    public static void main(String[] args) {
+        buildFile("abc");
+    }
     public static boolean buildFile(String repo) {
         File f = new File("./html/" + repo + ".html");
         try {
@@ -13,15 +17,20 @@ public class HtmlBuilder {
 
             writer.write("<!DOCTYPE html>\n");
             writer.write("<html>\n");
+            writer.write("<head>\n");
+            writer.write("<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">\n\n");
+            writer.write("<link rel=\"stylesheet\" href=\"style.css\">\n");
+            writer.write("<head/>\n");
             writer.write("<body>\n");
+            writer.write("<img src=\"g1.png\" alt=\"G1\" width=\"500\">");
 
-            writer.write("<table>\n");
+            writer.write("<table class=\"table table-striped\" \n");
             writer.write("<tr>\n");
-                writer.write(String.format("<th>%s</th>\n", "commitId"));
-                writer.write(String.format("<th>%s</th>\n", "branch"));
-                writer.write(String.format("<th>%s</th>\n", "timeStamp"));
-                writer.write(String.format("<th>%s</th>\n", "buildInfo"));
-                writer.write(String.format("<th>%s</th>\n", "testInfo"));
+                writer.write(String.format("<th>%s</th>\n", "Commit ID"));
+                writer.write(String.format("<th>%s</th>\n", "Branch"));
+                writer.write(String.format("<th>%s</th>\n", "Timestamp"));
+                writer.write(String.format("<th>%s</th>\n", "Build success"));
+                writer.write(String.format("<th>%s</th>\n", "Test success"));
             writer.write("</tr>\n");
 
 

b41ffc14016735477973c5c82badb28a70efd7aa
vluthman <vluthman@kth.se>
2022-02-16 14:22:58 +0100

    Fix CompileTest only clone repo once

--- src/test/java/CompileTest.java
+++ src/test/java/CompileTest.java
@@ -4,14 +4,13 @@ import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class CompileTest {
-    compile compile = new compile();
-    boolean cloneSucceeded = CloneRepo.cloneRepo("https://github.com/DD2480group1/assignment2/", "refs/heads/main");
+    private static compile compile = new compile();
+    private static boolean cloneSucceeded = CloneRepo.cloneRepo("https://github.com/DD2480group1/assignment2/", "refs/heads/main");
 
     //Tests if the cloning of the repo works
     @Test
     public void cloneTest(){
-        String errorMessage = "The code could not be cloned";
-        assertTrue(errorMessage, cloneSucceeded);
+        assertTrue(cloneSucceeded);
     }
 
     //Tests if the compile class returns an errorCode or not

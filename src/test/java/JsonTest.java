2d8d69085da4badf8ea35fdea43d5af4471be237
Isabel Redtzer <isabel.redtzer@gmail.com>
2022-03-08 16:47:09 +0100

    Added setUp to tests and documentation

--- src/test/java/JsonTest.java
+++ src/test/java/JsonTest.java
@@ -1,45 +1,67 @@
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.json.JsonObject;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * This JsonTest class tests that the Json-reader works and is reading the correct data.
+ * The data is read from a static file with the correct format.
+ *  @author  Isabel Redtzer
+ * */
 public class JsonTest {
+    static JsonUtil json;
+    static JsonObject jsonObject;
 
-    SkeletonCode skeletonCode = new SkeletonCode();
-    String test = skeletonCode.readFile("./src/main/java/test.json");
-    JsonUtil json = new JsonUtil();
-    JsonObject jsonObject = json.getJson(test);
+    /**
+     * setUp() accesses data from the static test file in src/main/java/test.json
+     * */
+    @BeforeClass
+    public static void setUp(){
+        SkeletonCode skeletonCode = new SkeletonCode();
+        String test = skeletonCode.readFile("./src/main/java/test.json");
+        json = new JsonUtil();
+        jsonObject = json.getJson(test);
+    }
 
-    //Tests if the RepoURL is retrieved correctly from test file
+    /**
+     * repoURLTest() tests that the repoURL is fetched correctly.
+     * */
     @Test
-    public void RepoURLTest(){
+    public void repoURLTest(){
         String expected = "https://github.com/DD2480group1/assignment2";
         String actual = json.getRepoUrl(jsonObject);
         String errorMessage = expected + " is not the same as " + actual;
         assertTrue(errorMessage, expected.equals(actual));
     }
 
-    //Tests if the email is retrieved correctly from test file
+    /**
+     * emailTest() tests that the email of the comitter is fetched correctly.
+     * */
     @Test
-    public void EmailTest(){
+    public void emailTest(){
         String expected = "rubbestads@gmail.com";
         String actual = json.getHeadCommitEmail(jsonObject);
         String errorMessage = expected + " is not the same as " + actual;
         assertTrue(errorMessage, expected.equals(actual));
     }
 
-    //Tests if the commit-ID is retrieved correctly from test file
+    /**
+     * headCommitIdTest() tests that the commit-ID is fetched correctly.
+     * */
     @Test
-    public void HeadCommitIdTest(){
+    public void headCommitIdTest(){
         String expected = "07402d78bf18007353352cbb21a6f7813470caf9";
         String actual = json.getHeadCommitId(jsonObject);
         String errorMessage = expected + " is not the same as " + actual;
         assertTrue(errorMessage, expected.equals(actual));
     }
 
-    //Tests if the Commit Message is retrieved correctly from test file
+    /**
+     * commitMsgTest() tests that the commit message is fetched correctly.
+     * */
     @Test
-    public void CommitMsgTest(){
+    public void commitMsgTest(){
         String expected = "test5";
         String actual = json.getCommitMsg(jsonObject);
         String errorMessage = expected + " is not the same as " + actual;

1109a1c8001463ac61650c1c550ce5d44cf0d658
vluthman <vluthman@kth.se>
2022-03-08 16:16:51 +0100

    add javadoc to class and method

--- src/main/java/CloneRepo.java
+++ src/main/java/CloneRepo.java
@@ -7,8 +7,17 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
-/*This function clones the code from the repo into a folder /repo to be able to test*/
+/**
+ * The class allows for cloning a repo so that the codebase can be built and
+ * tested for the continuous integration.
+ */
 public class CloneRepo {
+    /**
+     * This function clones the code from the repo into a folder at /repo
+     * @param url the url of the repo
+     * @param branch the branch to clone from
+     * @return whether the repo cloned successfully
+     */
     public static boolean cloneRepo(String url, String branch){
         String path = "./repo/";
         try {
